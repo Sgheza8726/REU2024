e8d66586fedbd628947fabf140b6820ef7a9b98a
Eve Fleisig <katchu11@gmail.com>
2024-07-18 17:14:21 +0000

    changes

--- perp_llama3.py
+++ perp_llama3.py
@@ -3,12 +3,12 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 from datasets import load_from_disk
 import evaluate
 
-#best checkpoint and tokenizer from fine-tuned model
+#best checkpoint and tokenizer
 model_name = "./results/checkpoint-13500"
 tokenizer = AutoTokenizer.from_pretrained('./fine-tuned-llama3-8b')
 model = AutoModelForCausalLM.from_pretrained(model_name)
 
-#load test dataset
+#test dataset
 test_dataset_path = '/home/efleisig/sams_reu/custom_huggingface_dataset/test'
 test_dataset = load_from_disk(test_dataset_path)
 
@@ -20,8 +20,15 @@ test_dataset = test_dataset.map(preprocess_function, batched=True)
 
 #function to compute perplexity
 def compute_perplexity(model, tokenizer, test_dataset):
-    perplexity = evaluate.load("perplexity")
-    results = perplexity.compute(model_id=model_name, dataset=test_dataset, add_special_tokens=False)
+    perplexity = evaluate.load("/home/efleisig/sams_reu/my_perplexity", module_type="metric")
+    predictions = [tokenizer.decode(ids, skip_special_tokens=True) for ids in test_dataset['input_ids']]
+    
+    #predictions in batches
+    for text in predictions:
+        perplexity.add_batch(predictions=[text])
+    
+    #compute perplexity
+    results = perplexity.compute(model_id=(model, tokenizer))
     return results['perplexity']
 
 #compute perplexity
